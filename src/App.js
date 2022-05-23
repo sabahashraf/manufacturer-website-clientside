7f9a01b62209b2cd5b8d895bb5b9ab0c44f32865
Sabah Sabrina Ashraf <sabahsabrina@yahoo.com>
2022-05-23 11:51:55 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Header from "./Pages/Shared/Header";
+import Blogs from "./Pages/Blogs/Blogs";
+import Portfolio from "./Pages/Porfolio/Portfolio";
+import Dashboard from "./Pages/DashBoard/Dashboard";
+import NotFound from "./Pages/NotFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="px-12 max-w-7xl">
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/portfolio" element={<Portfolio></Portfolio>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Blogs/Blogs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Blogs = () => {
+  return (
+    <div>
+      <h2 className="text-2xl text center">Blog page</h2>
+    </div>
+  );
+};
+
+export default Blogs;
