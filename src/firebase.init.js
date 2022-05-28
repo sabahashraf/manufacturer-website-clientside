31f1e2138b33122ad02b27592b79d7172fecd328
Sabah Sabrina Ashraf <sabahsabrina@yahoo.com>
2022-05-27 17:33:42 -0700

    firebase config added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,17 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAmiNkavCGVvCE8tAT98aDiMeDyx8MS62E",
-
-  authDomain: "power-painting-tools.firebaseapp.com",
-
-  projectId: "power-painting-tools",
-
-  storageBucket: "power-painting-tools.appspot.com",
-
-  messagingSenderId: "78560501095",
-
-  appId: "1:78560501095:web:d3a4b5170917b00ebea3c2",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
